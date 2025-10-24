760d6c740e5c5900b6e955314ad09bfa7250a1d6
Sujit Walmiki <sujitwalmiki.nbnssoe.comp@gmail.com>
2025-10-25 00:49:21 +0530

    Removed wrong Logger Package From Application

--- src/main/java/com/example/demo/JenkinsProjectApplication.java
+++ src/main/java/com/example/demo/JenkinsProjectApplication.java
@@ -1,28 +1,27 @@
 package com.example.demo;
 
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
-
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JenkinsProjectApplication {
 
-	public static Logger logger =   (Logger) LoggerFactory.getLogger(JenkinsProjectApplication.class);
+	public static Logger logger = LoggerFactory.getLogger(JenkinsProjectApplication.class);
+  
 			
 	public void init() {
 		
-		logger.log(Level.INFO, "Application Execution from logger ");
+		logger.info("Application Execution from logger");
 	}
 	
 	@PostConstruct
 	public static void main(String[] args) {
 		
-		logger.log(Level.INFO, "Application Execution from main Method ");
+		logger.info("Application Execution from main Method");
 		SpringApplication.run(JenkinsProjectApplication.class, args);
 	}
 
